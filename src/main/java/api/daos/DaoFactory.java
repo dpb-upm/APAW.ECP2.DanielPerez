1c3fb472bc6fc11ee8f81e9f169e7e8f75e19cb8
Daniel Pérez <>
2018-10-12 12:57:35 +0200

    - Creado DAO Factory

---
+++ src/main/java/api/daos/DaoFactory.java
@@ -0,0 +1,23 @@
+package api.daos;
+
+public abstract class DaoFactory {
+
+    private static DaoFactory factory = null;
+
+    public static DaoFactory getFactory() {
+        assert factory != null;
+        return factory;
+    }
+
+    public static void setFactory(DaoFactory factory) {
+        DaoFactory.factory = factory;
+    }
+
+    public abstract ArchivoDAO getArchivoDAO();
+
+    public abstract ServidorDAO getServidorDAO();
+
+    public abstract PropietarioDAO getPropietarioDAO();
+
+    public abstract SaaDAO getSalaDAO();
+}
