8a35cf17e57eac0bd165d1c7f72bbb2e85ec04cf
Daniel Pérez <>
2018-10-14 11:53:49 +0200

    - creados métodos para preparar la 3º api, obtener/get un propietario

--- src/main/java/api/businesscontroller/PropietarioBusinessController.java
+++ src/main/java/api/businesscontroller/PropietarioBusinessController.java
@@ -3,6 +3,9 @@ package api.businesscontroller;
 import api.daos.DaoFactory;
 import api.dtos.PropietarioDto;
 import api.entities.Propietario;
+import api.entities.Servidor;
+
+import java.util.Optional;
 
 public class PropietarioBusinessController {
 
@@ -10,5 +13,9 @@ public class PropietarioBusinessController {
         Propietario propietario = new Propietario(propietarioDto.getNombre(), propietarioDto.getEdad(), propietarioDto.getNacimiento());
         DaoFactory.getFactory().getPropietarioDAO().save(propietario);
         return propietario.getId();
+    }
+
+    public Optional<Servidor> read(String id) {
+        return DaoFactory.getFactory().getServidorDAO().read(id);
     }
 }
