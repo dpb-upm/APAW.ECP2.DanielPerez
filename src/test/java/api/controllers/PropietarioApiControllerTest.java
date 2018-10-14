b2f8a8fdcee2a11f92a236770f55de1f2f90640e
Daniel Pérez <>
2018-10-14 12:10:20 +0200

    3º API - Get Propietario - Creado test para BAD_REQUEST

--- src/test/java/api/controllers/PropietarioApiControllerTest.java
+++ src/test/java/api/controllers/PropietarioApiControllerTest.java
@@ -11,7 +11,9 @@ public class PropietarioApiControllerTest {
 
     @Test
     void crearPropietarioOK() {
-        HttpRequest request = HttpRequest.builder(PropietarioApiController.ADD_PROPIETARIO_SERVIDOR).body(new PropietarioDto("Daniel", 30, DateUtil.getFormatedDate("1988-05-10 00:00"))).post();
+        HttpRequest request = HttpRequest.builder(PropietarioApiController.ADD_PROPIETARIO_SERVIDOR)
+                .body(new PropietarioDto("Daniel", 30, DateUtil.getFormatedDate("1988-05-10 00:00")))
+                .post();
         HttpResponse httpResponse= new Client().submit(request);
         assertEquals(httpResponse.getStatus(), HttpStatus.OK);
         assertNotNull(httpResponse.getBody());
@@ -19,22 +21,47 @@ public class PropietarioApiControllerTest {
 
     @Test
     void crearPropietarioBadRequest() {
-        HttpRequest request = HttpRequest.builder(PropietarioApiController.ADD_PROPIETARIO_SERVIDOR).path("/q").body(1).post();
+        HttpRequest request = HttpRequest.builder(PropietarioApiController.ADD_PROPIETARIO_SERVIDOR)
+                .path("/q")
+                .body(1)
+                .post();
         HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
         assertEquals(exception.getHttpStatus(), HttpStatus.BAD_REQUEST);
     }
 
     @Test
     void obtenerPropietarioOK(){
-        HttpRequest request = HttpRequest.builder(PropietarioApiController.ADD_PROPIETARIO_SERVIDOR).body(new PropietarioDto("Daniel", 30, DateUtil.getFormatedDate("1988-05-10 00:00"))).post();
+        HttpRequest request = HttpRequest.builder(PropietarioApiController.ADD_PROPIETARIO_SERVIDOR)
+                .body(new PropietarioDto("Daniel", 30, DateUtil.getFormatedDate("1988-05-10 00:00")))
+                .post();
         HttpResponse httpResponse = new Client().submit(request);
         assertEquals(httpResponse.getStatus(), HttpStatus.OK);
         assertNotNull(httpResponse.getBody());
         String id = (String) httpResponse.getBody();
 
-        HttpRequest request2 = HttpRequest.builder(PropietarioApiController.GET_PROPIETARIO_SERVIDOR).body(id).get();
+        HttpRequest request2 = HttpRequest.builder(PropietarioApiController.GET_PROPIETARIO_SERVIDOR)
+                .body(id)
+                .get();
         HttpResponse httpResponse2 = new Client().submit(request2);
         assertEquals(httpResponse2.getStatus(), HttpStatus.OK);
         assertNotNull(httpResponse2.getBody());
+    }
+
+    @Test
+    void obtenerPropietarioBadRequest(){
+        HttpRequest request = HttpRequest.builder(PropietarioApiController.ADD_PROPIETARIO_SERVIDOR)
+                .body(new PropietarioDto("Daniel", 30, DateUtil.getFormatedDate("1988-05-10 00:00")))
+                .post();
+        HttpResponse httpResponse = new Client().submit(request);
+        assertEquals(httpResponse.getStatus(), HttpStatus.OK);
+        assertNotNull(httpResponse.getBody());
+        String id = (String) httpResponse.getBody();
+
+        HttpRequest request2 = HttpRequest.builder(PropietarioApiController.GET_PROPIETARIO_SERVIDOR)
+                .path("/id")
+                .body(id)
+                .get();
+        HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request2));
+        assertEquals(exception.getHttpStatus(), HttpStatus.BAD_REQUEST);
     }
 }
